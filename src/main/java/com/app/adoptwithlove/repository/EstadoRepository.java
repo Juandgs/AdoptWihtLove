d7d093c145b160632cbc7eba6ce267f50468c452
Juandgs <judigosa92@gmail.com>
2025-11-11 17:18:27 -0500

    Seeders

--- src/main/java/com/app/adoptwithlove/repository/EstadoRepository.java
+++ src/main/java/com/app/adoptwithlove/repository/EstadoRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.app.adoptwithlove.entity.Estado;
 
 public interface EstadoRepository extends JpaRepository<Estado, Long>  {
-    Optional<Estado> findById(Long id);
+    Estado findByNombreEstado(String nombreEstado);
     
 }
